f5588ecc9e7fbee67b6f2b9b181716afeacc19ee
BRivsC <bastian.rivas@sansano.usm.cl>
2025-04-23 17:33:05 -0400

    Eliminado código de ejemplo

--- Guia1/Pregunta2/modelo_omni_5ruedas.m
+++ Guia1/Pregunta2/modelo_omni_5ruedas.m
@@ -5,24 +5,13 @@
 % radios de las ruedas como D. Acá fueron cambiados para que se adecúen a
 % lo que se vio en clases.
 
-%% Omnidireccional 3 ruedas
-clear; close; clc; 
-%Creación de variables simbólicas
-syms r L theta
-%Cálculo de la cinemática directa respecto al sistema global
-A=[sqrt(3)/2 -1/2 -L; 0 1 -L; -sqrt(3)/2 -1/2 -L]
-B=[r 0 0; 0 r 0;0 0 r]
-J=simplify(inv(A)*B) %matriz Jacobiana directa
-Rdi=[cos(theta) -sin(theta) 0;sin(theta) cos(theta) 0;0 0 1];
-Jglobal=simplify(Rdi*J)
-%Cáculo de la cinemática inversa respecto al sistema global
-invJ=inv(J); %matriz Jacobiana inversa
-Rin=[cos(theta) sin(theta) 0;-sin(theta) cos(theta) 0;0 0 1];
-invJglobal=simplify(invJ*Rin)
 
-%% Lo mío: Omnidireccional 5 ruedas
+%% Omnidireccional 5 ruedas
 clear; close; clc; 
 %Creación de variables simbólicas
+% r: Radio de las ruedas
+% L: Distancia del centro del robot hacia la rueda
+% theta: Ángulo de pose del robot
 syms r L theta;
 %Cálculo de la cinemática directa respecto al sistema global
 % Ángulos de las ruedas
