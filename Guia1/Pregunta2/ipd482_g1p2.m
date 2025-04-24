fc94fcc38d084236ef5db2c311849cf88ead58fc
BRivsC <bastian.rivas@sansano.usm.cl>
2025-04-23 21:50:46 -0400

    Agregada visualización de perfiles de velocidad

--- Guia1/Pregunta2/ipd482_g1p2.m
+++ Guia1/Pregunta2/ipd482_g1p2.m
@@ -1,10 +1,8 @@
 %% Guía 1 IPD482, pregunta 2
 % Pregunta 2: Perfiles de velocidad de omnidireccional de 5 ruedas
 % Bastián Rivas
-
+clear; close all; clc;
 %% Generación del camino
-% Línea recta
-
 % Coordenadas de los puntos
 % Cada punto del camino está descrito por la posición (x,y) y el ángulo de
 % pose respecto al sistema de referencia externo.
@@ -43,73 +41,44 @@ camino = [x_linea y_linea ang];
 % theta_N es el ángulo de pose de la rueda N respecto a 
 % sistema de referencia externo
 % theta_1 = 0 + ang_robot;
-% theta_2 = 2*pi/5 + ang_robot;
-% theta_3 = 4*pi/5 + ang_robot;
-% theta_4 = 6*pi/5 + ang_robot;
-% theta_5 = 8*pi/5 + ang_robot;
+% theta_2 = 2*pi/5 + theta_1;
+% theta_3 = 4*pi/5 + theta_1;
+% theta_4 = 6*pi/5 + theta_1;
+% theta_5 = 8*pi/5 + theta_1;
 
-%Velocidad angular (señal de control): wn= vn/r,
-%Radio (en metros) de las ruedas según datos técnicos de Robotino:
-r=0.04;
-L=0.2; %Distancia del centro del robot hacia la rueda (en metros)
-
+% Parámetros
+r = 0.04; % Radio de las ruedas según datos técnicos de Robotino (en metros) 
+L = 0.2;  % Distancia del centro del robot hacia la rueda (en metros)
+dt = 0.5; % Tiempo de muestreo
 
 % Perfil de velocidades
-vCuad=zeros(5,21); % Cada columna corresponde a un instante de tiempo
-                   % Cada fila corresponde a una rueda
+vel_camino=zeros(5,MAX_PTS+1); % Cada columna corresponde a un instante de tiempo
+                               % Cada fila corresponde a una rueda
+                               % Se agrega un extra para el instante 0
 
-for k=1:20
+for k=1:MAX_PTS-1
     
-    theta_1=pi/3+ang(k); %nuevo ángulo de pose
-    theta_2=2*pi/3+theta_1;
-    theta_3=4*pi/3+theta_1;
+    theta_1 = ang(k); %nuevo ángulo de pose
+    theta_2 = 2*pi/5 + theta_1;
+    theta_3 = 4*pi/5 + theta_1;
+    theta_4 = 6*pi/5 + theta_1;
+    theta_5 = 8*pi/5 + theta_1;
+
     
     %Matriz del modelo omnidireccional con valores del instante k
-    A= 2/3*[-sin(theta_1),-sin(theta_1+theta_2),-sin(theta_1+theta_3);
-        cos(theta_1),cos(theta_1+theta_2),cos(theta_1+theta_3);
-        1/(2*R),1/(2*R),1/(2*R)];
+    Jglobal =[ 0.4*r*sin(ang(k)), 0.38*r*cos(ang(k)) + 0.12*r*sin(ang(k)), 0.24*r*cos(ang(k)) - 0.32*r*sin(ang(k)), -0.24*r*cos(ang(k)) - 0.32*r*sin(ang(k)),  0.12*r*sin(ang(k))-0.38*r*cos(ang(k));
+              -0.4*r*cos(ang(k)), 0.38*r*sin(ang(k)) - 0.12*r*cos(ang(k)), 0.32*r*cos(ang(k)) + 0.24*r*sin(ang(k)),  0.32*r*cos(ang(k)) - 0.24*r*sin(ang(k)), -0.12*r*cos(ang(k))-0.38*r*sin(ang(k));
+                      -(0.2*r)/L,                              -(0.2*r)/L,                              -(0.2*r)/L,                               -(0.2*r)/L,                             -(0.2*r)/L];
     
     %Obtener velocidad para ir al siguiente punto usando pseudoinversa
-    vCuad(:,k)=pinv(A)*(transpose(cuadrado(k+1,:)-cuadrado(k,:)))/dt;
+    vel_camino(:,k)=pinv(Jglobal)*(transpose(camino(k+1,:)-camino(k,:)))/dt;
     
 end
 
 % figure()
- tTotal=0:dt:(length(vCuad(1,:))-1)*dt;
- plot(tTotal,vCuad(1,:)/r,'r',tTotal,vCuad(2,:)/r,'g',tTotal,vCuad(3,:)/r,'b')
- title('Velocidades angulares para el camino cuadrado')
+ tTotal=0:dt:(length(vel_camino(1,:))-1)*dt;
+ plot(tTotal,vel_camino(1,:)/r,'r',tTotal,vel_camino(2,:)/r,'g',tTotal,vel_camino(3,:)/r,'b',tTotal,vel_camino(4,:)/r,'m',tTotal,vel_camino(5,:)/r,'k')
+ title('Velocidades angulares a lo largo del trayecto')
  xlabel('Tiempo (s)')
  ylabel('Velocidad angular (rad/s)')
- legend('w1','w2','w3')
-
-%c) 8-invertido
-
-vOcho=zeros(3,21);
-
-for k=1:20
-    
-    theta_1=pi/3+angOcho(k); %nuevo ángulo de pose
-    theta_2=2*pi/3+theta_1;
-    theta_3=4*pi/3+theta_1;
-    
-    %Matriz del modelo omnidireccional con valores del instante k
-    A= 2/3*[-sin(theta_1),-sin(theta_1+theta_2),-sin(theta_1+theta_3);
-        cos(theta_1),cos(theta_1+theta_2),cos(theta_1+theta_3);
-        1/(2*R),1/(2*R),1/(2*R)];
-    
-    %Obtener velocidad para ir al siguiente punto usando pseudoinversa
-vOcho(:,k)=pinv(A)*(transpose(ochoInvertido(k+1,:)-ochoInvertido(k,:)))/dt;
-    
-end
-
-% figure()
-% tTotal=0:dt:(length(vOcho(1,:))-1)*dt;
-% plot(tTotal,vOcho(1,:)/r,'r',tTotal,vOcho(2,:)/r,'g',
-% tTotal,vOcho(3,:)/r,'b')
-% title('Velocidades angulares para el camino ocho invertido')
-% xlabel('Tiempo (s)')
-% ylabel('Velocidad angular (rad/s)')
-% legend('w1','w2','w3')
-
-
-
+ legend('w1','w2','w3','w4','w5','orientation','horizontal')
